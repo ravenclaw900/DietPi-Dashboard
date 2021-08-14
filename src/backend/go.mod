cbc6ecf0988d249c64f760764cbda739d2118c53
ravenclaw900 <50060110+ravenclaw900@users.noreply.github.com>
2021-08-14 14:08:06 -0500

    Init basic web server

---
+++ src/backend/go.mod
@@ -0,0 +1,5 @@
+module github.com/ravenclaw900/DietPi-Dashboard
+
+go 1.16
+
+require github.com/gorilla/mux v1.8.0
---
+++ src/backend/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", handler).Methods("GET")
+
+	http.ListenAndServe(":8080", r)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
