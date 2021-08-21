dbb653ba70f313610fb0d98e6d5bd298b5870f74
ravenclaw900 <50060110+ravenclaw900@users.noreply.github.com>
2021-08-21 18:48:21 -0500

    Improve layout to use flexbox

--- src/frontend/src/App.svelte
+++ src/frontend/src/App.svelte
@@ -4,7 +4,7 @@
     import Test from "./pages/Test.svelte";
     export let url = "";
 
-    var socket;
+    let socket;
     const socketMessageListener = (e) => {
         console.log(e.data)
     }
@@ -35,17 +35,20 @@
   @tailwind utilities;
 </style>
   
-<main>
+<main class="min-h-screen -m-2 flex">
     <Router url="{url}">
-        <div class="float-left bg-gray-900 h-96 w-1/6 mr-2 -ml-2 h-screen">
+        <div class="bg-gray-900 w-1/6 flex-grow">
             <div class="h-12 bg-lime-500 text-2xl flex items-center justify-center">DietPi Dashboard</div>
             <Link to="/" class="text-gray-400 no-underline hover:bg-gray-800 flex items-center pl-2 h-10 text-xl font-sans">Home</Link>
             <Link to="test" class="text-gray-400 no-underline hover:bg-gray-800 flex items-center pl-2 h-10 text-xl font-sans">Test</Link>
         </div>
-        <div class="bg-lime-400 h-12 -mt-2 -mr-2"></div>
-        <div>
-            <Route path="test" component="{Test}" /> 
-            <Route path="/" component="{Home}"></Route>
+        <div class="w-5/6 flex flex-col flex-grow min-h-full">
+            <header class="bg-lime-400 h-12">Test</header>
+            <div class="bg-gray-100 flex-grow">
+                <Route path="test" component="{Test}" /> 
+                <Route path="/" component="{Home}" />
+            </div>
+            <footer class="border-t bg-gray-200 border-gray-300 h-10">DietPi Dashboard</footer>
         </div>
     </Router>
 </main>
--- src/frontend/src/pages/Home.svelte
+++ src/frontend/src/pages/Home.svelte
@@ -2,7 +2,64 @@
     let pageName="Home Page";
 </script>
 
-<main class="bg-lime-50 h-screen">
-    <h1> {pageName}!</h1>
-    <p>Welcome this is my <b>{pageName}</b></p>
+<main class="">
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
+    <h1> Test </h1>
 </main>
