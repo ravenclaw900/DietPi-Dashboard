b7c20951530ec34f180c06af3aaac7c26d2f9e86
ravenclaw900 <50060110+ravenclaw900@users.noreply.github.com>
2021-11-20 09:27:25 -0600

    refactor(backend): don't load unnecessary config file parameters

--- src/backend/src/config.rs
+++ src/backend/src/config.rs
@@ -36,28 +36,36 @@ pub fn config() -> Config {
     let port: u16 = cfg.get("port").unwrap_or(&Toml::Num(8088.0)).num() as u16;
 
     let tls = cfg.get("tls").unwrap_or(&Toml::Bool(false));
-    let cert = cfg
-        .get("cert")
-        .unwrap_or(&Toml::Str(String::new()))
-        .str()
-        .to_string();
-    let key = cfg
-        .get("key")
-        .unwrap_or(&Toml::Str(String::new()))
-        .str()
-        .to_string();
+    let cert = String::new();
+    let key = String::new();
+    if tls == &Toml::Bool(true) {
+        let cert = cfg
+            .get("cert")
+            .unwrap_or(&Toml::Str(String::new()))
+            .str()
+            .to_string();
+        let key = cfg
+            .get("key")
+            .unwrap_or(&Toml::Str(String::new()))
+            .str()
+            .to_string();
+    }
 
     let pass = cfg.get("pass").unwrap_or(&Toml::Bool(false));
-    let hash = cfg
-        .get("hash")
-        .unwrap_or(&Toml::Str(String::new()))
-        .str()
-        .to_string();
-    let secret = cfg
-        .get("secret")
-        .unwrap_or(&Toml::Str(String::new()))
-        .str()
-        .to_string();
+    let hash = String::new();
+    let secret = String::new();
+    if pass == &Toml::Bool(true) {
+        hash = cfg
+            .get("hash")
+            .unwrap_or(&Toml::Str(String::new()))
+            .str()
+            .to_string();
+        secret = cfg
+            .get("secret")
+            .unwrap_or(&Toml::Str(String::new()))
+            .str()
+            .to_string();
+    }
 
     Config {
         port,
