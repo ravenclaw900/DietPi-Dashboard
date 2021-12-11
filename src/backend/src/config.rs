bdde6c46927b240a6a0e42ebb630488fab2f66e1
ravenclaw900 <50060110+ravenclaw900@users.noreply.github.com>
2021-12-11 09:07:34 -0600

    feat(config): add nodes config setting
    Also switch to `toml` crate, instead of using nanoserde parser

--- src/backend/src/config.rs
+++ src/backend/src/config.rs
@@ -1,4 +1,4 @@
-use nanoserde::{Toml, TomlParser};
+use toml::Value;
 
 pub struct Config {
     pub port: u16,
@@ -10,72 +10,99 @@ pub struct Config {
     pub pass: bool,
     pub hash: String,
     pub secret: String,
+    pub nodes: Vec<String>,
 }
 
 pub fn config() -> Config {
     let mut cfgpath = std::env::current_exe().unwrap();
     cfgpath.set_file_name("config.toml");
-    let cfg = TomlParser::parse(
-        &match std::fs::read_to_string(cfgpath) {
-            Err(ref e) if e.kind() == std::io::ErrorKind::NotFound => {
-                std::fs::write("config.toml", "").unwrap();
-                String::new()
-            }
-            Ok(cfg) => cfg,
-            Err(e) => {
-                panic!("Config file could not be read: {}", e);
-            }
+    let cfg = &match std::fs::read_to_string(cfgpath) {
+        Err(ref e) if e.kind() == std::io::ErrorKind::NotFound => {
+            std::fs::write("config.toml", "").unwrap();
+            String::new()
         }
-        .lines()
-        .filter(|line| !line.starts_with('#')) // Remove comments, parser can't handle them
-        .map(|line| line.to_string() + "\n")
-        .collect::<String>(),
-    )
+        Ok(cfg) => cfg,
+        Err(e) => {
+            panic!("Config file could not be read: {}", e);
+        }
+    }
+    .parse::<Value>()
     .expect("Invalid config file");
 
     #[allow(clippy::cast_sign_loss)]
     #[allow(clippy::cast_possible_truncation)]
-    let port: u16 = cfg.get("port").unwrap_or(&Toml::Num(5252.0)).num() as u16;
+    let port: u16 = cfg
+        .get("port")
+        .unwrap_or(&Value::Integer(5252))
+        .as_integer()
+        .unwrap() as u16;
 
-    let tls = cfg.get("tls").unwrap_or(&Toml::Bool(false));
+    let tls = cfg
+        .get("tls")
+        .unwrap_or(&Value::Boolean(false))
+        .as_bool()
+        .unwrap();
     let mut cert = String::new();
     let mut key = String::new();
-    if tls == &Toml::Bool(true) {
+    if tls {
         cert = cfg
             .get("cert")
-            .unwrap_or(&Toml::Str(String::new()))
-            .str()
+            .unwrap_or(&Value::String(String::new()))
+            .as_str()
+            .unwrap()
             .to_string();
         key = cfg
             .get("key")
-            .unwrap_or(&Toml::Str(String::new()))
-            .str()
+            .unwrap_or(&Value::String(String::new()))
+            .as_str()
+            .unwrap()
             .to_string();
     }
 
-    let pass = cfg.get("pass").unwrap_or(&Toml::Bool(false));
+    let pass = cfg
+        .get("pass")
+        .unwrap_or(&Value::Boolean(false))
+        .as_bool()
+        .unwrap();
+
     let mut hash = String::new();
     let mut secret = String::new();
-    if pass == &Toml::Bool(true) {
+    if pass {
         hash = cfg
             .get("hash")
-            .unwrap_or(&Toml::Str(String::new()))
-            .str()
+            .unwrap_or(&Value::String(String::new()))
+            .as_str()
+            .unwrap()
             .to_string();
         secret = cfg
             .get("secret")
-            .unwrap_or(&Toml::Str(String::new()))
-            .str()
+            .unwrap_or(&Value::String(String::new()))
+            .as_str()
+            .unwrap()
             .to_string();
     }
 
+    let mut nodes = Vec::new();
+
+    for i in cfg
+        .get("nodes")
+        .unwrap_or(&Value::Array(Vec::new()))
+        .as_array()
+        .unwrap()
+    {
+        nodes.push(i.as_str().unwrap().to_string());
+    }
+
+    dbg!(&nodes);
+
     Config {
         port,
-        tls: tls == &Toml::Bool(true),
+        tls,
         cert,
         key,
-        pass: pass == &Toml::Bool(true),
+        pass,
         hash,
         secret,
+        nodes,
     }
 }
