4a95a790743af239c1f9c98c1e9677a995a266f9
ravenclaw900 <50060110+ravenclaw900@users.noreply.github.com>
2021-12-20 11:28:47 -0600

    build(vite): only run replace on production

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -4,24 +4,26 @@ import { replaceCodePlugin } from "vite-plugin-replace";
 import windi from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte(), windi({}), replaceCodePlugin({
-    replacements: [
-      {
-        from: /__PACKAGE_VERSION__/g,
-        to: process.env.npm_package_version,
-      },
-      {
-        from: /5252/g,
-        to: "window.location.port",
-      },
-    ]
-  })],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          xterm: ['xterm', 'xterm-addon-attach', 'xterm-addon-fit']
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [svelte(), windi({}), replaceCodePlugin({
+      replacements: [
+        {
+          from: /__PACKAGE_VERSION__/g,
+          to: process.env.npm_package_version,
+        },
+        {
+          from: /5252/g,
+          to: process.env.NODE_ENV === 'production' ? "window.location.port" : "5252",
+        },
+      ]
+    })],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            xterm: ['xterm', 'xterm-addon-attach', 'xterm-addon-fit']
+          }
         }
       }
     }
