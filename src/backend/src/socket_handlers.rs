80fd1d98a9e946bf88972875a0feecad65a70518
ravenclaw900 <50060110+ravenclaw900@users.noreply.github.com>
2022-04-03 10:46:14 -0500

    feat(terminal): allow manual login

--- src/backend/src/socket_handlers.rs
+++ src/backend/src/socket_handlers.rs
@@ -138,13 +138,17 @@ pub async fn term_handler(socket: warp::ws::WebSocket) {
         }
     }
 
+    let mut pre_cmd = std::process::Command::new("/bin/login");
+    let pre_cmd = pre_cmd.env("TERM", "xterm");
+
     let cmd = Arc::new(RwLock::new(
-        // Use hardcoded bash here until we have better support for other shells
-        std::process::Command::new("/bin/login")
-            .env("TERM", "xterm")
-            .args(&["-f", &crate::CONFIG.terminal_user])
-            .spawn_pty(None)
-            .unwrap(),
+        if crate::CONFIG.terminal_user == "manual" {
+            pre_cmd
+        } else {
+            pre_cmd.args(&["-f", &crate::CONFIG.terminal_user])
+        }
+        .spawn_pty(None)
+        .unwrap(),
     ));
     let cmd_write = Arc::clone(&cmd);
     let cmd_clone = Arc::clone(&cmd);
