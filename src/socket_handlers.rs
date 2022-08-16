1504fd3504553e8b9f103f3f04752c97fbe55f3e
ravenclaw900 <50060110+ravenclaw900@users.noreply.github.com>
2022-08-15 21:13:01 -0500

    style(backend): clippy nursery lints

--- src/socket_handlers.rs
+++ src/socket_handlers.rs
@@ -17,8 +17,8 @@ enum TokenState {
 }
 
 impl TokenState {
-    fn as_bool(&self) -> bool {
-        if let TokenState::ValidToken = self {
+    const fn as_bool(&self) -> bool {
+        if let Self::ValidToken = self {
             return true;
         }
         false
@@ -276,7 +276,7 @@ pub async fn term_handler(
                 } else {
                     tracing::debug!("Exiting terminal");
                     // Stop bash by writing "exit", since it won't respond to a SIGTERM
-                    let _write = cmd.pty().write_all("exit\n".as_bytes());
+                    let _write = cmd.pty().write_all(b"exit\n");
                     break;
                 }
             }
