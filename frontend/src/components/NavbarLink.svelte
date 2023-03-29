d26bf3348ebb330e814cf68bea1128d451efc8b6
ravenclaw900 <50060110+ravenclaw900@users.noreply.github.com>
2023-03-28 19:16:03 -0500

    refactor(frontend): remove commented-out code from NavbarLink

--- frontend/src/components/NavbarLink.svelte
+++ frontend/src/components/NavbarLink.svelte
@@ -1,7 +1,3 @@
-<script lang="ts">
-    //export let class1: string;
-</script>
-
 <div
     class="text-gray-400 no-underline hover:bg-gray-800 flex items-center h-10 text-xl font-sans whitespace-nowrap cursor-pointer"
 >
