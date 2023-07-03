be15317c52af2501743235b4954e3b74e74f132b
ravenclaw900 <50060110+ravenclaw900@users.noreply.github.com>
2023-07-03 15:28:14 -0500

    chore(frontend): migrate to Svelte 4
    Run automated migration tool. Also change prettier formatting for every file that isn't .svelte

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,7 +5,7 @@ import "vite/modulepreload-polyfill";
 import App from "./App.svelte";
 
 const app = new App({
-    target: document.getElementById("app") as HTMLElement,
+  target: document.getElementById("app") as HTMLElement,
 });
 
 export default app;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,26 +5,26 @@ import extractorSvelte from "@unocss/extractor-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-    return {
-        plugins: [
-            unocss({
-                extractors: [extractorSvelte()],
-            }),
-            svelte(),
-        ],
-        build: {
-            manifest: true,
-            rollupOptions: {
-                input: "src/main.ts",
-                output: {
-                    manualChunks: {
-                        xterm: ["xterm", "xterm-addon-attach", "xterm-addon-fit"],
-                    },
-                },
-            },
+  return {
+    plugins: [
+      unocss({
+        extractors: [extractorSvelte()],
+      }),
+      svelte(),
+    ],
+    build: {
+      manifest: true,
+      rollupOptions: {
+        input: "src/main.ts",
+        output: {
+          manualChunks: {
+            xterm: ["xterm", "xterm-addon-attach", "xterm-addon-fit"],
+          },
         },
-        define: {
-            __PACKAGE_VERSION__: JSON.stringify(process.env.npm_package_version),
-        },
-    };
+      },
+    },
+    define: {
+      __PACKAGE_VERSION__: JSON.stringify(process.env.npm_package_version),
+    },
+  };
 });
